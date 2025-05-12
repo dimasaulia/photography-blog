e821996aa47a367aa9d1ce436094d8cf82effea9
Dimas Aulia Fachrudin <dimasauliafachrudin@gmail.com>
2025-05-12 10:39:20 +0700

    fix log

--- monitoring/controllers/monitoring.controllers_impl.go
+++ monitoring/controllers/monitoring.controllers_impl.go
@@ -4,6 +4,7 @@ import (
 	h "blog/monitoring/helpers"
 	"blog/provider/config"
 	"fmt"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -25,8 +26,7 @@ func (m *MonitoringControllersImpl) GetServerMetrics(c *fiber.Ctx) error {
 	appEnv := env.GetString("ENV")
 
 	serverStat := h.GetServerStatistic()
-	serverMetrics := fmt.Sprintf(`
-	# HELP program_memory Show how much program use system memory
+	serverMetrics := fmt.Sprintf(`# HELP program_memory Show how much program use system memory
     # TYPE program_memory gauge
     resident_set_size_bytes{program="be-blog-life", env="%v"} %v
     heap_limit_bytes{program="be-blog-life", env="%v"} %v
@@ -43,5 +43,13 @@ func (m *MonitoringControllersImpl) GetServerMetrics(c *fiber.Ctx) error {
 		appEnv, 0,
 	)
 
-	return c.Send([]byte(serverMetrics))
+	// Trim spaces from each line
+	// Split the metrics string into lines, trim each line, and join them back
+	lines := strings.Split(serverMetrics, "\n")
+	for i, line := range lines {
+		lines[i] = strings.TrimSpace(line)
+	}
+	trimmedMetrics := strings.Join(lines, "\n")
+
+	return c.Send([]byte(trimmedMetrics))
 }
